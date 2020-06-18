ee598e010a54159ffb9aa6184224b7568609ebb8
Finn Francis <finnfrancis123@gmail.com>
2020-06-18 10:34:50 +0100

    Installed rubocop and rubocop-performace gems
    Why?
    - Allows us to keep our syntax consistent.
    - Rubocop performance will prevent us from making obvious performance mistakes.
    
    How?
    - Installed the gems.
    - Added a configuration to remove the features that we don't want.
    - Fixed existing rubocop errors.

--- lib/ui/shell.rb
+++ lib/ui/shell.rb
@@ -12,29 +12,27 @@ module Assist
       def start
         exit_in = Float::INFINITY
         loop do
-          begin
-            break if exit_in < 1
+          break if exit_in < 1
 
-            input = ::Readline.readline(@prompt.to_s, '')
+          input = ::Readline.readline(@prompt.to_s, '')
 
-            case command = input[/\w+/]
-            when 'exit'
-              exit_in = 0
-            when 'cd'
-              Dir.chdir(input.sub(command, '').strip)
-              puts Dir.getwd
-            else
-              puts "#{input}: command not found" if system(input).nil? && !input.empty?
-            end
-
-            exit_in = Float::INFINITY unless exit_in.zero?
-
-          # Interrupt is called when we press ctrl+c, rescuing it will allow
-          # us to keep usual commandline ctrl+c functionality
-          rescue Interrupt
-            exit_in = exit_in == 1 ? 0 : 1
-            print "\n"
+          case command = input[/\w+/]
+          when 'exit'
+            exit_in = 0
+          when 'cd'
+            Dir.chdir(input.sub(command, '').strip)
+            puts Dir.getwd
+          else
+            puts "#{input}: command not found" if system(input).nil? && !input.empty?
           end
+
+          exit_in = Float::INFINITY unless exit_in.zero?
+
+        # Interrupt is called when we press ctrl+c, rescuing it will allow
+        # us to keep usual commandline ctrl+c functionality
+        rescue Interrupt
+          exit_in = exit_in == 1 ? 0 : 1
+          print "\n"
         end
 
         puts 'Exiting...'
